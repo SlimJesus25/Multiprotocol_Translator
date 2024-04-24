3d2605f7db848cb99e8cf44087702fa77b9b1b57
ricardo <1210828@isep.ipp.+pt>
2024-04-24 20:06:24 +0100

    Improved JavaRepository efficiency changing the database data structure from ArrayList to TreeMap.

--- arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/QoSDatabase/JavaRepository.java
+++ arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/QoSDatabase/JavaRepository.java
@@ -2,24 +2,27 @@ package eu.arrowhead.application.skeleton.consumer.classes.QoSDatabase;
 
 import eu.arrowhead.application.skeleton.consumer.classes.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class JavaRepository implements Repository {
 
-    List<String> database = new ArrayList<>();
+    private Map<String, Integer> database = new TreeMap<>();
+    private int count = 0;
 
     @Override
     public boolean registerNewMessage(String messageId) {
+
+        // This won't be a huge problem, but it needs to be solved!
         if (database.size() > Constants.MAX_UNIQUE_MESSAGES) {
             database.remove(0);
         }
-        return database.add(messageId);
+        return database.put(messageId, count++) == null;
     }
 
     @Override
     public boolean messageExists(String messageId) {
-        return database.contains(messageId);
+        return database.containsKey(messageId);
     }
 
     @Override
