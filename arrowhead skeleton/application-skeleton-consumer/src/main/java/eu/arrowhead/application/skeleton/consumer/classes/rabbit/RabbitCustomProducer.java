e644dc93e5cd19490bfb6f57b9b4474e27e9601f
Ricardo Venâncio <1210828@isep.ipp.pt>
2024-04-27 18:44:55 +0100

    Implemented asynchronously handling for ACK and NACK messages for each QoS level.

--- arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/rabbit/RabbitCustomProducer.java
+++ arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/rabbit/RabbitCustomProducer.java
@@ -1,30 +1,30 @@
 package eu.arrowhead.application.skeleton.consumer.classes.rabbit;
 
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import common.ConnectionDetails;
 import common.IProducer;
 import eu.arrowhead.application.skeleton.consumer.classes.QoSDatabase.ExactlyOnceProducerHelper;
 import eu.arrowhead.application.skeleton.consumer.classes.Utils;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RabbitCustomProducer extends IProducer {
 
     private org.slf4j.Logger log = LoggerFactory.getLogger(RabbitCustomProducer.class);
-    private ExactlyOnceProducerHelper exactlyOnceProducerHelper = new ExactlyOnceProducerHelper();
     private Channel channel;
     private ConnectionFactory factory;
     private RabbitConsumerSettings settings;
     private int numberOfMessages = 1;
-    private final Object mutex = new Object();
     private long utilsID;
+    private ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
 
     public RabbitCustomProducer(ConnectionDetails connectionDetails, Map<String,String> settings) {
         super(connectionDetails, settings);
@@ -49,17 +49,62 @@ public class RabbitCustomProducer extends IProducer {
             int prefetchCount = 0;
             boolean global = true;
 
+            ConfirmCallback confirmCallback = (sequenceNumber, multiple) -> {
+                // System.out.println("acked messages: " + sequenceNumber + ", multiple: " + multiple);
+                if(multiple){
+                    int amount = 0;
+                    Iterator<Map.Entry<Long, String>> it = outstandingConfirms.descendingMap().entrySet().iterator();
+                    while(it.hasNext() || outstandingConfirms.containsKey(sequenceNumber)){
+                        if(it.next().getKey() <= sequenceNumber) {
+                            it.remove();
+                            amount++;
+                        }
+                    }
+                    numberOfMessages += amount;
+                }else{
+                    outstandingConfirms.remove(sequenceNumber);
+                    numberOfMessages++;
+                }
+            };
+
+            ConfirmCallback nackedConfirmCallback = (sequenceNumber, multiple) -> {
+                    // System.out.println("nacked messages: " + sequenceNumber + " " + multiple);
+                    if(multiple){
+                        ConcurrentNavigableMap<Long, String> nAcked = outstandingConfirms.headMap(sequenceNumber, true);
+                        nAcked.forEach((id, message) -> produceMessage(settings.getQueue(), message));
+                    }else{
+                        produceMessage(settings.getQueue(), outstandingConfirms.get(sequenceNumber));
+                    }
+                };
+
             if(settings.getQos() == 0){
+
+                channel.addConfirmListener(confirmCallback, (sequence, multiple) -> {
+
+                });
                 channel.basicQos(prefetchSize, prefetchCount, global);
             }else if(settings.getQos() == 1){
+
+                channel.confirmSelect();
+
+                channel.addConfirmListener(confirmCallback, // message was confirmed.
+                        nackedConfirmCallback); // message was nack-ed.
+
                 prefetchCount = 10;
                 channel.basicQos(prefetchSize, prefetchCount, global);
             }else if(settings.getQos() == 2){
+
+                channel.confirmSelect();
+
+                // Sequence Number -> number that identifies the confirmed or nack-ed messages.
+                // Multiple -> if false, only one message is confirmed/nack-ed. if true, all messages with a lower or
+                // equal sequence number are confirmed/nack-ed.
+                channel.addConfirmListener(confirmCallback, nackedConfirmCallback);
+
                 prefetchCount = 1;
                 channel.basicQos(prefetchSize, prefetchCount, global);
             }
 
-            // channel.queueDeclare(settings.getQueue(), false, false, false, null);
         } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
@@ -68,13 +113,9 @@ public class RabbitCustomProducer extends IProducer {
     @Override
     public void produce(String topic, String message) {
 
-        boolean confirmed = false;
-
         if (numberOfMessages == 1) {
             utilsID = Utils.initializeCounting();
         }
-
-        numberOfMessages++;
 
         String queueName;
 
@@ -85,57 +126,11 @@ public class RabbitCustomProducer extends IProducer {
             queueName = settings.getQueue();
         }
 
-        try {
-            String messageId;
-            if (settings.isRandomId()) {
-                messageId = UUID.randomUUID().toString();
-            } else {
-                messageId = message;
-            }
 
-            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
-                    .messageId(messageId)
-                    .build();
-
-            AtomicBoolean flag = new AtomicBoolean(true);
-
-            if (settings.getQos() == 0) {
-                produceMessage(queueName, properties, message);
-            }else if(settings.getQos() == 1){
-
-                channel.confirmSelect();
-
-                channel.addConfirmListener((sequenceNumber, multiple) -> {
-                    synchronized (this.mutex) {
-                        flag.set(false);
-                    }
-                    }, (sequenceNumber, multiple) -> {
-                    synchronized (this.mutex) {
-                        flag.set(false);
-                    }
-
-                });
-
-                int retries = 5;
-                while(retries-- >= 0){
-                    synchronized (this.mutex) {
-                        if(flag.get()) {
-                            produceMessage(queueName, properties, message);
-                        }
-                    }
-                }
-            } else {
-                channel.confirmSelect();
-                while (!confirmed) {
-                    produceMessage(queueName, properties, message);
-                    confirmed = channel.waitForConfirms();
-                }
-            }
-
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
+            produceMessage(queueName, message);
+        if(settings.getQos() == 0) {
+            numberOfMessages++;
         }
-
         if(this.numberOfMessages == 50000f || this.numberOfMessages == 25000f || this.numberOfMessages == 75000f){
             Utils.halfCounting(utilsID);
         }
@@ -146,10 +141,11 @@ public class RabbitCustomProducer extends IProducer {
         }
     }
 
-    private void produceMessage(String queueName, AMQP.BasicProperties properties, String message) {
+    private void produceMessage(String queueName, String message) {
 
         try {
-            channel.basicPublish("", queueName, properties, message.getBytes());
+            outstandingConfirms.put(channel.getNextPublishSeqNo(), message);
+            channel.basicPublish("", queueName, null, message.getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
