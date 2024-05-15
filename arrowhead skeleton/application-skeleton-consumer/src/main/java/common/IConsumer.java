bac6fcf07780f4c8160b94038fffefe943f967fe
Ricardo Venâncio <1210828@isep.ipp.pt>
2024-05-15 17:41:05 +0100

    Added TODO with efficiency tips.

--- arrowhead skeleton/application-skeleton-consumer/src/main/java/common/IConsumer.java
+++ arrowhead skeleton/application-skeleton-consumer/src/main/java/common/IConsumer.java
@@ -27,6 +27,9 @@ public abstract class IConsumer implements Runnable {
         return connectionDetails;
     }
 
+    // TODO: create a thread for each producer instead of using the main one for each producer.
+    // It needs to be analyzed if LatchCount needs to be used in order to make the main thread wait for all threads do the work (- efficiency, + secure)
+    // Or if it's ok to create threads and moving on with processing the messages from the external publisher (+ efficiency, - secure)
     public void OnMessageReceived(String topic, String message) {
 
         for (IProducer producer : producerList) {
