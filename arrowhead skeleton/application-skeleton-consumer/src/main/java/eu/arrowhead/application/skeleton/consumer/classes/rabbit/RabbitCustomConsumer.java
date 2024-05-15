b3f333695ce60721c6b563848a2e5b3e86b5b379
Ricardo Venâncio <1210828@isep.ipp.pt>
2024-05-15 17:42:30 +0100

    Converted global variables into local and removed redundancy and unnecessary code.

--- arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/rabbit/RabbitCustomConsumer.java
+++ arrowhead skeleton/application-skeleton-consumer/src/main/java/eu/arrowhead/application/skeleton/consumer/classes/rabbit/RabbitCustomConsumer.java
@@ -5,13 +5,10 @@ import common.ConnectionDetails;
 import common.IConsumer;
 import common.IProducer;
 import eu.arrowhead.application.skeleton.consumer.classes.QoSDatabase.JavaRepository;
-import org.apache.kafka.common.utils.Java;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
@@ -19,18 +16,9 @@ import java.util.concurrent.TimeoutException;
 public class RabbitCustomConsumer extends IConsumer {
 
     private Channel channel;
-
     private final JavaRepository qosRepository = new JavaRepository();
-
-    private ConnectionFactory factory;
-
     private final Logger logger = LogManager.getLogger(RabbitCustomConsumer.class);
-
-    private RabbitProducerSettings settings;
-
-    private String queueName;
-
-    AMQP.Queue.DeclareOk queue;
+    private final RabbitProducerSettings settings;
 
     public RabbitCustomConsumer(ConnectionDetails connectionDetails, List<IProducer> producer, Map<String, String> settings) {
         super(connectionDetails, producer, settings);
@@ -47,7 +35,7 @@ public class RabbitCustomConsumer extends IConsumer {
     }
 
     private void connect() {
-        factory = settings.getRabbitSettings().getConnectionFactory();
+        ConnectionFactory factory = settings.getRabbitSettings().getConnectionFactory();
         factory.setHost(this.getConnectionDetails().getAddress());
         factory.setUsername("admin");
         factory.setPassword("admin");
@@ -55,16 +43,13 @@ public class RabbitCustomConsumer extends IConsumer {
             Connection connection = factory.newConnection();
             channel = connection.createChannel();
 
-            logger.info("AMQP Consumer connected at " + getConnectionDetails().getAddress() + ":" + getConnectionDetails().getPort());
+            logger.info("AMQP Consumer connected at {}:{}", getConnectionDetails().getAddress(), getConnectionDetails().getPort());
         } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
 
-
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-
-
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             OnMessageReceived(settings.getRoutingKey(),message);
             lastMessage = message;
             numberOfMessages++;
@@ -72,7 +57,8 @@ public class RabbitCustomConsumer extends IConsumer {
 
         try {
 
-            if (!settings.getExchange().equals("")) {
+            String queueName;
+            if (!settings.getExchange().isEmpty()) {
                 queueName = channel.queueDeclare().getQueue();
                 channel.exchangeDeclare(settings.getExchange(), BuiltinExchangeType.DIRECT);
                 channel.queueBind(queueName, settings.getExchange(), settings.getRoutingKey());
@@ -123,6 +109,4 @@ public class RabbitCustomConsumer extends IConsumer {
             throw new RuntimeException(e);
         }
     }
-
-
 }
